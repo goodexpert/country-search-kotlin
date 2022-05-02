e0bd7245434152f76fad61b3c71cc703e5a1da60
Steve Park <seongwuk.park@gmail.com>
2022-05-02 16:28:59 +1200

    Fix a freezing UI issue with AsyncTask

--- app/src/main/java/com/atlassian/androidinterview/CountrySearchActivity.kt
+++ app/src/main/java/com/atlassian/androidinterview/CountrySearchActivity.kt
@@ -1,5 +1,6 @@
 package com.atlassian.androidinterview
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,20 +11,36 @@ class CountrySearchActivity : AppCompatActivity() {
 
     private val repository = CountryRepository()
 
+    private var searchTask: MyAsyncTask? = null
+    private val countryAdapter = CountryAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val searchEditText: EditText = findViewById(R.id.searchEditText)
         val countryRecyclerView: RecyclerView = findViewById(R.id.countryRecyclerView)
-        val countryAdapter = CountryAdapter()
 
         countryRecyclerView.layoutManager = LinearLayoutManager(this)
         countryRecyclerView.adapter = countryAdapter
         countryAdapter.submitList(repository.getAllCountries())
 
         searchEditText.afterTextChanged { text ->
-            countryAdapter.submitList(repository.getFilteredCountries(text))
+            // Should run this on other thread using coroutine or asynctask or thread
+            searchTask?.cancel(false)
+            searchTask = MyAsyncTask().apply {
+                execute(text)
+            }
+        }
+    }
+
+    inner class MyAsyncTask : AsyncTask<String, Void, List<Country>>() {
+        override fun doInBackground(vararg params: String): List<Country> {
+            return repository.getFilteredCountries(params[0])
+        }
+
+        override fun onPostExecute(result: List<Country>?) {
+            countryAdapter.submitList(result)
         }
     }
 }
