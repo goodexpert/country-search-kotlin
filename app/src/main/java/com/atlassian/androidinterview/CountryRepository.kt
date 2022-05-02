60e140f66c7948b244eeb77ac3a483e614575604
Steve Park <seongwuk.park@gmail.com>
2022-05-02 16:23:53 +1200

    Fix the country repository issue

--- app/src/main/java/com/atlassian/androidinterview/CountryRepository.kt
+++ app/src/main/java/com/atlassian/androidinterview/CountryRepository.kt
@@ -7,17 +7,20 @@ class CountryRepository(private val localeProvider: LocaleProvider = AndroidLoca
      * [startsWith] value ignoring upper/lower case.
      */
     fun getFilteredCountries(startsWith: String) = getAllCountries()
-            .filter { country -> country.name.startsWith(startsWith) }
+            .filter { country -> country.name.startsWith(startsWith, true) }
 
     /**
+     * Should remove an empty name and duplicate name from the list.
      * @return list of countries sorted by ASC.
      */
     fun getAllCountries(): List<Country> {
-        val countries = mutableListOf<Country>()
+        val countries = mutableSetOf<Country>()
         for (locale in localeProvider.provideLocales()) {
             countries.add(Country(locale.displayCountry))
         }
         return countries
+            .filter { it.name.isNotEmpty() }
+            .sortedBy { it.name }
     }
 
 }
